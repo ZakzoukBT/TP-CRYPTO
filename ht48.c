d90bdafbb3cf700ccc8b31dff0a717d1b3366f08
Nizar Azzouzi <azznizar9@gmail.com>
2024-03-13 16:28:49 +0100

    Question 1

--- ht48.c
+++ ht48.c
@@ -230,5 +230,41 @@ void ht48(uint64_t blen, const uint8_t m[blen], uint8_t h[static 6])
 
 void printhash(const uint8_t h[static 6])
 {
-    printf("%02X%02X%02X%02X%02X%02X", h[0], h[1], h[2], h[3], h[4], h[5]);
+    printf("%02X%02X%02X%02X%02X%02X\n", h[0], h[1], h[2], h[3], h[4], h[5]);
+}
+
+/*
+* Input k: a 48-bit key stored as an array of 6 bytes
+* Input blen: the byte length of m, stored on 64 bits
+* Input m: the message to be hashed, whose length is required to be an integral number of bytes
+* Input h: placeholder for the 48-bit resulting tag, to be stored as an array of 6 bytes. Must have been allocated by the caller.
+* Output: void, h is overwritten with the result ht48(m||k)
+* Warning: the key bytes in k must be appended _in order_ (k[0] first)
+*/
+void smht48(const uint8_t k[static 6], uint64_t blen, const uint8_t m[blen], uint8_t h[static 6]){
+    uint64_t size = blen + 6;
+    uint8_t concat_message[size];
+    for (int i=0; i <blen; i++)
+    {
+        concat_message[i] = m[i];
+    } 
+    for (int i = 0; i < 6; i++){
+        int tmp = blen + i;
+        concat_message[tmp] = k[i];
+    }
+
+    ht48(sizeof(concat_message), concat_message, h);
+    printhash(h);
+}
+
+int main(void){
+    uint8_t keys[6] = {0, 1, 2, 3, 4, 5};
+    uint8_t message[6] = {9, 8, 7, 6, 5, 4};
+    uint8_t keys_two[6] = {0xE4, 0x16, 0x9F, 0x12, 0xD3, 0xBA};
+    uint8_t h[6];
+    
+    smht48(keys,sizeof(message), message,h);
+    smht48(keys_two,sizeof(message),message,h);
+
+    return 0;
 }
